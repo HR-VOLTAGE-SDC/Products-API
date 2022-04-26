501455a21c1dff77d16be7f1f7ca6571ce985edd
amyKwak <yoomikwak@gmail.com>
2022-04-26 02:19:10 -0700

    feat: add unit test

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -13,17 +13,25 @@ describe('GET /products', function() {
 
 describe('GET /products/:id', function() {
   it('responds with a status of 200 for a product', async function() {
-    const id = 123;
+    const id = 1000011;
     const response = await request(app)
       .get(`/products/${id}`)
       .set('Accept', 'application/json')
     expect(response.status).toEqual(200);
   });
+
+  it('responds with correct product name', async function() {
+    const id = 1000011;
+    const response = await request(app)
+      .get(`/products/${id}`)
+      .set('Accept', 'application/json')
+    expect(response._body.name).toEqual('Evangeline Shoes');
+  });
 });
 
 describe('GET /products/:id/styles', function() {
   it('responds with a status of 200 for styles', async function() {
-    const id = 123;
+    const id = 1000011;
     const response = await request(app)
       .get(`/products/${id}/styles`)
       .set('Accept', 'application/json')
@@ -33,7 +41,7 @@ describe('GET /products/:id/styles', function() {
 
 describe('GET /products/:id/related', function() {
   it('responds with a status of 200 for related products', async function() {
-    const id = 123;
+    const id = 1000011;
     const response = await request(app)
       .get(`/products/${id}/styles`)
       .set('Accept', 'application/json')
